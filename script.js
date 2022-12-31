5e78541c254d549eaace4520cccae2e40dfa53c9
AP696 <addyphilips66@gmail.com>
2022-12-31 00:42:51 +0000

    1

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById("nav");
+const toggleIcon = document.getElementById("toggle-icon");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+const textBox = document.getElementById("text-box");
+
+//Switch Theme Dynamically
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+//Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
