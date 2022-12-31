ab9c510775ce8f6a70fc0288c1a645d92e2bc772
AP696 <addyphilips66@gmail.com>
2022-12-31 15:03:41 +0000

    complete

--- script.js
+++ script.js
@@ -35,12 +35,25 @@ function lightMode() {
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
     lightMode();
   }
 }
 
 //Event Listener
 toggleSwitch.addEventListener("change", switchTheme);
+
+// Check local Storage for theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if(currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
